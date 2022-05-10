ca9c94675e1c1dda90659a61a100d6184ccd7834
bellcastro <33166356+bellcastro@users.noreply.github.com>
2022-05-10 20:55:35 -0300

    Menu dropdown

--- src/components/organisms/header/index.jsx
+++ src/components/organisms/header/index.jsx
@@ -1,12 +1,64 @@
 import * as Styles from './styles.js'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
 
 export default function Header() {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Styles.Container>
       <Styles.Text>ClassOverflow</Styles.Text>
-      <Styles.Avatar>
-        <p>D</p>
-      </Styles.Avatar>
+      <Styles.Icone>
+
+      <div>
+      <React.Fragment>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <Avatar>D</Avatar>
+          </IconButton>
+        </Tooltip>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+      </React.Fragment>
+    </div>
+      </Styles.Icone>
     </Styles.Container>
   )
 }
