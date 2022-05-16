8ed2b1ecef07236fd07c9a9b759225b01c41a7f2
rafaelsiq <rafaelsqf2@gmail.com>
2022-05-16 16:48:21 -0300

    Adicionando o campo tag no post da pagina principal

--- src/components/molecules/leftMenu/styles.js
+++ src/components/molecules/leftMenu/styles.js
@@ -1,10 +1,25 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const TagContent = styled.div`
   justify-content: space-around;
-  padding-top: 50px;
-  padding-left: 35px;
   height: 100vh;
-  font-size: 20px;
+  font-size: 16px;
   color: black;
+  top:50px;
+  --rti-bg: #fff;
+    --rti-border: #ccc;
+    --rti-main: #3182ce;
+    --rti-radius: 0.375rem;
+    --rti-s: 0.5rem;
+    --rti-tag: #edf2f7;
+    --rti-tag-remove: #e53e3e;
+    align-items: center;
+    bg: var(--rti-bg);
+    border: 1px solid var(--rti-border);
+    border-radius: var(--rti-radius);
+    display: flex;
+    flex-wrap: wrap;
+    gap: var(--rti-s);
+    line-height: 1.4;
+    padding: var(--rti-s);
 `
