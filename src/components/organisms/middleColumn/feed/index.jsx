e2017cba9c550aafefd7f59aab26636cd987501a
drzbraz <drz.braz@gmail.com>
2022-05-25 18:40:34 -0300

    add mock

--- src/components/organisms/middleColumn/feed/index.jsx
+++ src/components/organisms/middleColumn/feed/index.jsx
@@ -145,7 +145,7 @@ export default function Feed({ posts, createPost }) {
                 {post.topic && post.topic.map((topic, index) => <Styles.Badge key={index}>{topic}</Styles.Badge>)}
               </Styles.Badges>
               <h3>{post.ownerName}</h3>
-              <p className="date">{post.createdAt ? new Date(post.createdAt).toLocaleDateString() : '25/05/2022'}</p>
+              <p className="date">{post.createdAt ? new Date(post.createdAt).toLocaleDateString() : '--/--/----'}</p>
             </Styles.RightColumn>
           </Styles.FeedPost>
         ))}
